594830ce665bbeb15cb901f0bc581a8bdcee5975
George <georgii.kashin@gmail.com>
2019-12-20 10:26:28 +0300

    Initial Commit

---
+++ Online-Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Online-Store
+//
+//  Created by Георгий Кашин on 20.12.2019.
+//  Copyright © 2019 Georgii Kashin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
